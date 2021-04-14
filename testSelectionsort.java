7c2e5cfd98c869a3f38c6a1ee8519b01f40be9eb
owen <orowader@umich.edu>
2021-04-14 19:59:15 -0400

    fix error and commit positive and negative tests

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -13,7 +13,7 @@ public class testSelectionSort {
 
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -21,29 +21,55 @@ public class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
-        /** add tests to check for this unit test **/
+
+				SelectionSort sort = new SelectionSort();
+
+        arr = sort.basicSelectionSort(arr);
+
+        for(int i = 0; i < 4; ++i) {
+        	assertEquals(Sortedarr[i], arr[i]);
+        }
     }
-    
+
     public void testNegative(){
-        
-        /** Test data contains negative values only **/
+
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = -9;
+			arr[2] = -7;
+			arr[3] = -10;
+			arr[4] = -2;
+
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -10;
+			Sortedarr[1] = -9;
+			Sortedarr[2] = -8;
+			Sortedarr[3] = -7;
+			Sortedarr[4] = -2;
+
+			SelectionSort sort = new SelectionSort();
+
+			arr = sort.basicSelectionSort(arr);
+
+			for(int i = 0; i < 4; ++i) {
+				assertEquals(Sortedarr[i], arr[i]);
+			}
     }
-    
+
     public void testMixed(){
-        
+
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
+
         /** Test data contains duplicates **/
     }
 
