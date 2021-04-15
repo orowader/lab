dfa59cbfbf89a48cdc4be838a18753c2bba94eca
owen <orowader@umich.edu>
2021-04-14 20:03:06 -0400

    fix sort and commit duplicate and mixed test

--- testSelectionsort.java
+++ testSelectionsort.java
@@ -13,7 +13,7 @@ public class testSelectionSort {
 
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -21,30 +21,71 @@ public class testSelectionSort {
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
-        
+
         /** Test data contains negative values only **/
     }
-    
+
     public void testMixed(){
-        
-        /** Test data contains with both positive, negative and zeros **/
+
+			int[] arr = new int[5];
+			arr[0] = -8;
+			arr[1] = 9;
+			arr[2] = 0;
+			arr[3] = -10;
+			arr[4] = 2;
+
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = -10;
+			Sortedarr[1] = -8;
+			Sortedarr[2] = 0;
+			Sortedarr[3] = 2;
+			Sortedarr[4] = 9;
+
+			SelectionSort sort = new SelectionSort();
+
+			arr = sort.basicSelectionSort(arr);
+
+			for(int i = 0; i < 4; ++i) {
+				assertEquals(Sortedarr[i], arr[i]);
+			}
     }
-    
+
     public void testDuplicates(){
-        
-        /** Test data contains duplicates **/
+
+			int[] arr = new int[5];
+			arr[0] = 8;
+			arr[1] = 10;
+			arr[2] = 10;
+			arr[3] = 7;
+			arr[4] = 2;
+
+			int[] Sortedarr = new int[5];
+			Sortedarr[0] = 2;
+			Sortedarr[1] = 7;
+			Sortedarr[2] = 8;
+			Sortedarr[3] = 10;
+			Sortedarr[4] = 10;
+
+			SelectionSort sort = new SelectionSort();
+
+			arr = sort.basicSelectionSort(arr);
+
+			for(int i = 0; i < 4; ++i) {
+				assertEquals(Sortedarr[i], arr[i]);
+			}
+	}
     }
 
 
